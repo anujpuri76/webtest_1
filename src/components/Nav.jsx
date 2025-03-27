38c70a73db490563e6dda65673352775044ef355
Gagan-Kaushik <gagankaushik0812@gmail.com>
2025-03-27 12:09:02 +0530

    update nav and del header

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,45 @@ import React from 'react'
 import './Nav.css'
 const Nav = () => {
   return (
-    <div className='nav'>
-      <marquee behavior="alternate" direction="right">
-        <h1>NavBar</h1>
-      </marquee>
-    </div>
-  )
-}
+
+     <header className="head">
+      <nav className="navbar">
+  
+        <div className="logo">
+          <a href="#">EDGECUT</a>
+        </div>
+
+    
+        <ul className="nav-links">
+          <li>
+            <a href="#" className="active">
+              HOME
+            </a>
+          </li>
+          <li>
+            <a href="#">ABOUT</a>
+          </li>
+          <li>
+            <a href="#">FURNITURES</a>
+          </li>
+          <li>
+            <a href="#">BLOG</a>
+          </li>
+          <li>
+            <a href="#">CONTACT US</a>
+          </li>
+        </ul>
+
+
+        <div className="right-section">
+          <a href="#" className="login">
+            LOGIN
+          </a>
+          <span className="search-icon">🔍</span>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Nav
